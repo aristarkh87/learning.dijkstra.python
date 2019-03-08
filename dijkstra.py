ffbd8d2bd4115ceaa9e2d998d80c3ffa99426736
Oleg Dolgikh <aristarkh@aristarkh.net>
2019-03-08 15:40:13 +0500

    Optimize code and result

--- dijkstra.py
+++ dijkstra.py
@@ -6,16 +6,10 @@ try:
 except IndexError:
     initial_node = "1"
 
-def get_inf(graph):
-    inf = 0
-    for edges in graph.values():
-        for weight in edges.values():
-            inf += weight
-    return inf + 1
 
-def get_current_node(labeled_graph, passed_nodes, inf):
+def get_current_node(labeled_graph, passed_nodes):
     current_node = None
-    current_node_label = inf
+    current_node_label = float("inf")
     for node, label in labeled_graph.items():
         if node not in passed_nodes:
             if label < current_node_label:
@@ -23,20 +17,27 @@ def get_current_node(labeled_graph, passed_nodes, inf):
                 current_node_label = label
     return current_node
 
+
+def print_result(labeled_graph):
+    print("{}\t{}".format("Node", "Path"))
+    for node, label in labeled_graph.items():
+        print("{}\t{}".format(node, label))
+
+
 if __name__ == "__main__":
-    inf = get_inf(origin_graph)
+    inf = float("inf")
     labeled_graph = { key: inf for key in origin_graph }
     labeled_graph[initial_node] = 0
-    passed_nodes = list()
     current_node = initial_node
+    passed_nodes = list()
 
     while current_node is not None:
         for neighbour, weight in origin_graph[current_node].items():
             if neighbour not in passed_nodes:
-                new_weight = weight + labeled_graph[current_node]
-                if new_weight < labeled_graph[neighbour]:
-                    labeled_graph[neighbour] = new_weight
+                new_label = weight + labeled_graph[current_node]
+                if new_label < labeled_graph[neighbour]:
+                    labeled_graph[neighbour] = new_label
         passed_nodes.append(current_node)
-        current_node = get_current_node(labeled_graph, passed_nodes, inf)
+        current_node = get_current_node(labeled_graph, passed_nodes)
 
-    print(labeled_graph)
+    print_result(labeled_graph)
